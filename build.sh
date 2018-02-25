995c697345a5a3da6596f4234479ced156d30765
Alex Kersten <alex@kersten.email>
2018-02-25 01:00:01 -0600

    directory structure changes and new font

--- build.sh
+++ build.sh
@@ -13,8 +13,9 @@ then
     mkdir -p static/css/bin
 fi
 
-sass src/scss/bulma.scss static/css/bin/bulma.css
-sass src/scss/cutekitchen.scss static/css/bin/cutekitchen.css
+#sass src/scss/bulma.scss static/css/bin/bulma.css
+#sass src/scss/cutekitchen.scss static/css/bin/cutekitchen.css
+sass --update src/scss:static/css/bin
 
 echo "-----------------------------------------------------------------------------------"
 echo "Build complete - run 'python3 cute-kitchen.py' to start the server."
