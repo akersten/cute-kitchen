f3977f0eb3740a56e3d7b94a1a1b7778ad7edbf5
Alex Kersten <alex@kersten.email>
2018-02-24 22:51:04 -0600

    Add icon font, splash page skeleton, color scheme

--- cute-kitchen.py
+++ cute-kitchen.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
+from src import route
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+route.init_routes(app)
 
 if __name__ == '__main__':
     app.run()
