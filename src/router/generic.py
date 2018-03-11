be6c4dc6ce9e0aec5d5cc5acf31839ad31a3478a
Alex Kersten <alex@kersten.email>
2018-03-10 20:41:20 -0600

    Add terms, license, and privacy policy

--- src/router/generic.py
+++ src/router/generic.py
@@ -13,6 +13,30 @@ def splash():
     return generic_path_render("outside/splash/splash.html")
 
 
+def privacy():
+    """
+    The privacy policy for CuteKitchen.
+    :return: The rendered template.
+    """
+    return generic_path_render("outside/privacy/privacy.html")
+
+
+def terms():
+    """
+    The terms of use for CuteKitchen.
+    :return: The rendered template.
+    """
+    return generic_path_render("outside/terms/terms.html")
+
+
+def license():
+    """
+    The license for CuteKitchen.
+    :return: The rendered template.
+    """
+    return generic_path_render("outside/license/license.html")
+
+
 def app():
     """
     The main application path for CuteKitchen.
